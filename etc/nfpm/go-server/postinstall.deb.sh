c263697c2d8375601236c4702bd8f1d2d4eee179
David Thorpe <djt@mutablelogic.com>
2021-09-28 13:46:44 +0200

    Updated

--- etc/nfpm/go-server/postinstall.deb.sh
+++ etc/nfpm/go-server/postinstall.deb.sh
@@ -3,6 +3,11 @@
 # Add nginx user
 id -u nginx &>/dev/null || useradd --system nginx
 
+# Change permissions on var folder
+if [ -d "/opt/go-server/var" ] ; then
+  chown -R nginx "/opt/go-server/var"
+fi
+
 # Enable service
 systemctl link /opt/go-server/etc/go-server.service
 systemctl enable go-server.service
