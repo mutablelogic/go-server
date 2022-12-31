9bfaaed113f00181e539f972a016cbef070a8e59
David Thorpe <djt@mutablelogic.com>
2022-12-31 15:22:59 +0000

    Updated static file serving

--- pkg/httpserver/static/static.go
+++ pkg/httpserver/static/static.go
@@ -3,14 +3,17 @@ package static
 import (
 	"context"
 	"fmt"
+	"io/fs"
+	"net/http"
 	"sync"
 
 	// Package imports
-
+	iface "github.com/mutablelogic/go-server"
 	task "github.com/mutablelogic/go-server/pkg/task"
 	plugin "github.com/mutablelogic/go-server/plugin"
+
 	// Namespace imports
-	//. "github.com/djthorpe/go-errors"
+	. "github.com/djthorpe/go-errors"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,7 +23,8 @@ type static struct {
 	task.Task
 	sync.RWMutex
 
-	label string
+	label   string
+	handler http.Handler
 }
 
 var _ plugin.Gateway = (*static)(nil)
@@ -29,9 +33,16 @@ var _ plugin.Gateway = (*static)(nil)
 // LIFECYCLE
 
 // Create a new static serving task
-func NewWithPlugin(p Plugin, label string) (*static, error) {
+func NewWithPlugin(p iface.Plugin, fs fs.FS) (*static, error) {
 	static := new(static)
-	static.label = label
+	static.label = p.Label()
+
+	// Create the handler
+	if fs == nil {
+		return nil, ErrBadParameter.With("fs")
+	} else {
+		static.handler = http.FileServer(http.FS(fs))
+	}
 
 	// Return success
 	return static, nil
@@ -66,5 +77,7 @@ func (static *static) Description() string {
 
 // Register routes for router
 func (static *static) RegisterHandlers(parent context.Context, router plugin.Router) {
-	// TODO
+	// GET /
+	//   Return list of prefixes and their handlers
+	router.AddHandler(parent, nil, static.handler.ServeHTTP)
 }
