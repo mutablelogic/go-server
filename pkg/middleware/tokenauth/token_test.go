5a196496c27addea8eb2a85e5a304688f63a7985
David Thorpe <djt@mutablelogic.com>
2024-05-31 13:50:37 +0200

    Updated tests

--- pkg/middleware/tokenauth/token_test.go
+++ pkg/middleware/tokenauth/token_test.go
@@ -8,6 +8,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	ScopeRead  = "read"
+	ScopeWrite = "write"
+)
+
 func Test_token_001(t *testing.T) {
 	assert := assert.New(t)
 	// Create a token with 100 bytes and no expiry
@@ -31,18 +36,18 @@ func Test_token_002(t *testing.T) {
 func Test_token_003(t *testing.T) {
 	assert := assert.New(t)
 	// Create a token with 100 bytes and one scope
-	token := tokenauth.NewToken(100, 0, tokenauth.ScopeRead)
+	token := tokenauth.NewToken(100, 0, ScopeRead)
 	assert.NotNil(token)
-	assert.True(token.IsScope(tokenauth.ScopeRead))
-	assert.False(token.IsScope(tokenauth.ScopeWrite))
+	assert.True(token.IsScope(ScopeRead))
+	assert.False(token.IsScope(ScopeWrite))
 	t.Log(token)
 }
 
 func Test_token_004(t *testing.T) {
 	assert := assert.New(t)
 	// Create a token with 100 bytes and one scope
-	token := tokenauth.NewToken(100, -1, tokenauth.ScopeRead)
+	token := tokenauth.NewToken(100, -1, ScopeRead)
 	assert.NotNil(token)
-	assert.False(token.IsScope(tokenauth.ScopeRead))
+	assert.False(token.IsScope(ScopeRead))
 	t.Log(token)
 }
