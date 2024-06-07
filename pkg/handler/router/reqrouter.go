da1e15f7f63b34d09938d5336d60cde53c48dfc3
David Thorpe <djt@mutablelogic.com>
2024-06-07 13:15:17 +0200

    Go back to previous

--- pkg/handler/router/reqrouter.go
+++ pkg/handler/router/reqrouter.go
@@ -105,7 +105,7 @@ func (router *reqs) AddHandler(ctx context.Context, path string, handler http.Ha
 func (router *reqs) AddHandlerRe(ctx context.Context, path *regexp.Regexp, handler http.HandlerFunc, methods ...string) *route {
 	// Add any middleware to the handler
 	middleware := Middleware(ctx)
-	//slices.Reverse(middleware)
+	slices.Reverse(middleware)
 	for _, middleware := range middleware {
 		handler = middleware.Wrap(ctx, handler)
 	}
