6a4e4c26116c23ee65dab18d68ea42e51cea7a7d
David Thorpe <djt@mutablelogic.com>
2024-06-07 11:44:43 +0200

    Added CreateCA endpoint

--- pkg/handler/certmanager/endpoints.go
+++ pkg/handler/certmanager/endpoints.go
@@ -8,11 +8,16 @@ import (
 	// Packages
 	server "github.com/mutablelogic/go-server"
 	router "github.com/mutablelogic/go-server/pkg/handler/router"
+	"github.com/mutablelogic/go-server/pkg/httprequest"
 	httpresponse "github.com/mutablelogic/go-server/pkg/httpresponse"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
 // TYPES
+
+type reqCreateCA struct {
+	CommonName string `json:"name"`
+}
 
 // Check interfaces are satisfied
 var _ server.ServiceEndpoints = (*certmanager)(nil)
@@ -26,6 +31,7 @@ const (
 
 var (
 	reRoot = regexp.MustCompile(`^/?$`)
+	reCA   = regexp.MustCompile(`^/ca/?$`)
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -37,14 +43,40 @@ func (service *certmanager) AddEndpoints(ctx context.Context, r server.Router) {
 	// Methods: GET
 	// Scopes: read
 	// Description: Return all existing certificates
-	r.AddHandlerFuncRe(ctx, reRoot, service.ListCerts, http.MethodGet).(router.Route).
+	r.AddHandlerFuncRe(ctx, reRoot, service.reqListCerts, http.MethodGet).(router.Route).
 		SetScope(service.ScopeRead()...)
+
+	// Path: /ca
+	// Methods: POST
+	// Scopes: write
+	// Description: Create a new certificate authority
+	r.AddHandlerFuncRe(ctx, reCA, service.reqCreateCA, http.MethodPost).(router.Route).
+		SetScope(service.ScopeWrite()...)
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 
 // Get all certificates
-func (service *certmanager) ListCerts(w http.ResponseWriter, r *http.Request) {
+func (service *certmanager) reqListCerts(w http.ResponseWriter, r *http.Request) {
 	httpresponse.JSON(w, service.List(), http.StatusOK, jsonIndent)
+}
+
+// Create a new certificate authority
+func (service *certmanager) reqCreateCA(w http.ResponseWriter, r *http.Request) {
+	var req reqCreateCA
+
+	// Get the request
+	if err := httprequest.Read(r, &req); err != nil {
+		httpresponse.Error(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Create the CA
+	if ca, err := service.CreateCA(req.CommonName); err != nil {
+		httpresponse.Error(w, http.StatusInternalServerError, err.Error())
+	} else {
+		httpresponse.JSON(w, ca, http.StatusOK, jsonIndent)
+	}
 }
