c7156e061cf7ea240072762bc86ec1b8a35ed4d2
David Thorpe <djt@mutablelogic.com>
2024-06-08 15:51:37 +0200

    Started adding the frontend code

---
+++ npm/router/src/index.js
@@ -0,0 +1,5 @@
+// This file defines all the styles and elements used for the web components
+
+// Bootstrap
+import './esbuild';
+import './main';
---
+++ npm/router/src/main.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+  // Initialize the application here
+  console.log('App loaded');
+});
