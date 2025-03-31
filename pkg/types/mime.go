b4948ff936434393a3c180230a59bf8278a6019a
David Thorpe <djt@mutablelogic.com>
2025-03-31 10:50:00 +0200

    Added TextStream

--- pkg/types/mime.go
+++ pkg/types/mime.go
@@ -8,15 +8,16 @@ import (
 // GLOBALS
 
 const (
-	ContentTypeJSON      = "application/json"
-	ContentTypeXML       = "application/xml"
-	ContentTypeRSS       = "application/rss+xml"
-	ContentTypeBinary    = "application/octet-stream"
-	ContentTypeCSV       = "text/csv"
-	ContentTypeTextXml   = "text/xml"
-	ContentTypeTextPlain = "text/plain"
-	ContentTypeFormData  = "multipart/form-data"
-	ContentTypeAny       = "*/*"
+	ContentTypeJSON       = "application/json"
+	ContentTypeXML        = "application/xml"
+	ContentTypeRSS        = "application/rss+xml"
+	ContentTypeBinary     = "application/octet-stream"
+	ContentTypeCSV        = "text/csv"
+	ContentTypeTextXml    = "text/xml"
+	ContentTypeTextPlain  = "text/plain"
+	ContentTypeTextStream = "text/event-stream"
+	ContentTypeFormData   = "multipart/form-data"
+	ContentTypeAny        = "*/*"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
