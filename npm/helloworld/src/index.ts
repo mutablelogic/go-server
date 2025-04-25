d7d134eb62bb9a03b44d160f75e35d5874ae5345
David Thorpe <djt@mutablelogic.com>
2025-04-25 23:54:55 +0200

    updated

---
+++ npm/helloworld/src/index.ts
@@ -0,0 +1,15 @@
+// Import web components
+import './wc/App.ts'
+import { GoogleAuth} from './wc/GoogleAuth.js'
+
+// Live reload for esbuild
+document.addEventListener("DOMContentLoaded", () => {
+    new EventSource('/esbuild').addEventListener('change', () => location.reload())
+});
+
+// Initialize the app
+document.addEventListener("DOMContentLoaded", () => {
+    const auth = new GoogleAuth('121760808688-hbiibnih1trt2vrokhrta17jgeuagp4k.apps.googleusercontent.com');
+
+    auth.render(document.getElementById('login') as HTMLElement);
+});
