b22cd29f20e52dd875578efbec8fd1e349a7eeb1
David Thorpe <djt@mutablelogic.com>
2025-04-28 08:46:58 +0200

    Added version

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -34,6 +34,7 @@ type CLI struct {
 	auth.UserCommands
 	auth.TokenCommands
 	auth.AuthCommands
+	VersionCommands
 }
 
 ///////////////////////////////////////////////////////////////////////////////
