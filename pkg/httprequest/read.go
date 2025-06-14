2ad066a144123b0bfd531e3a5bc808000a569fc3
David Thorpe <djt@mutablelogic.com>
2025-06-14 08:33:18 +0200

    Added multipart reader

--- pkg/httprequest/read.go
+++ pkg/httprequest/read.go
@@ -5,10 +5,19 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"reflect"
 
 	// Packages
-	"github.com/mutablelogic/go-server/pkg/httpresponse"
+	gomultipart "github.com/mutablelogic/go-client/pkg/multipart"
+	httpresponse "github.com/mutablelogic/go-server/pkg/httpresponse"
 	types "github.com/mutablelogic/go-server/pkg/types"
+)
+
+///////////////////////////////////////////////////////////////////////////////
+// GLOBALS
+
+const (
+	FormDataMaxMemory = 256 << 20 // 256 MB
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -28,6 +37,8 @@ func Read(r *http.Request, v interface{}) error {
 		return readJson(r, v)
 	case types.ContentTypeTextPlain:
 		return readString(r, v)
+	case types.ContentTypeFormData:
+		return readFormData(r, v)
 	}
 
 	// Cannot handle this content type
@@ -65,4 +76,49 @@ func readString(r *http.Request, v any) error {
 	default:
 		return httpresponse.ErrInternalError.Withf("cannot read %T as string", v)
 	}
+}
+
+var (
+	typeFile = reflect.TypeOf(gomultipart.File{})
+)
+
+func readFormData(r *http.Request, v any) error {
+	if err := r.ParseMultipartForm(FormDataMaxMemory); err != nil {
+		return err
+	}
+	if r.MultipartForm == nil {
+		return httpresponse.ErrBadRequest.With("Missing form data")
+	}
+
+	// Set non-file fields
+	if err := Query(r.MultipartForm.Value, v); err != nil {
+		return err
+	}
+	// Set file fields - we only support one file per field
+	for key, values := range r.MultipartForm.File {
+		if len(values) == 0 {
+			continue
+		}
+		// Get the first file for the field
+		value, err := writableFieldForName(v, key)
+		if err != nil {
+			return err
+		}
+		switch value.Type() {
+		case typeFile:
+			body, err := values[0].Open()
+			if err != nil {
+				return errBadRequest.Withf("cannot open file %q: %v", values[0].Filename, err)
+			}
+			value.Set(reflect.ValueOf(gomultipart.File{
+				Path: values[0].Filename,
+				Body: body,
+			}))
+		default:
+			return httpresponse.ErrBadRequest.Withf("cannot set field %q of type %s", key, value.Type())
+		}
+	}
+
+	// Return success
+	return nil
 }
